358798d650f63cccf213a2ce48ea85989eeb68bf
Newton Busters <newtonbustersftc@gmail.com>
2017-10-22 20:26:33 -0500

    Brandon: tested VuMarks, created autonomous op mode, which detects which VuMark is visible.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpMode_Relic.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.content.SharedPreferences;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+/**
+ * Created by Brandon on 10/22/2017.
+ */
+
+@Autonomous(name="AutonomousOpMode", group ="Main")
+public class AutonomousOpMode_Relic extends LinearOpMode {
+
+    VuforiaLocalizer vuforia;
+    VuforiaTrackables relicTrackables;
+    VuforiaTrackable relicTemplate;
+
+    @Override public void runOpMode() {
+
+        relicTemplate = vuforiaInitialize();
+
+        telemetry.addData(">", "Press Play to start");
+        telemetry.update();
+        waitForStart();
+
+        relicTrackables.activate(); // Starts looking for VuMarks
+
+        RelicRecoveryVuMark vuMark = findVuMark();
+
+        while (opModeIsActive()){
+
+        }
+
+    }
+
+    private VuforiaTrackable vuforiaInitialize(){
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        //TODO: In the line below, delete "cameraMonitorViewId" before using to speed up program.
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+        // get Vuforia license key
+        SharedPreferences prefs = AutonomousOptions.getSharedPrefs(hardwareMap);
+        parameters.vuforiaLicenseKey = prefs.getString("vuforiaLicenseKey", null);
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+        relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
+        VuforiaTrackable relicTemplate = relicTrackables.get(0);
+        relicTemplate.setName("relicVuMarkTemplate"); // can help in debugging; otherwise not necessary
+        return relicTemplate;
+    }
+
+    private RelicRecoveryVuMark findVuMark (){
+        RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
+        long vuStartTime = System.currentTimeMillis();
+        while (vuMark == RelicRecoveryVuMark.UNKNOWN){
+            vuMark = RelicRecoveryVuMark.from(relicTemplate);
+            telemetry.addData("VuMark", "not visible");
+            telemetry.update();
+            // If the seeing takes too long (10 seconds here), then just go with putting a block in the left one.
+            if (System.currentTimeMillis() - vuStartTime > 10*1000){
+                vuMark = RelicRecoveryVuMark.LEFT;
+            }
+        }
+        telemetry.addData("VuMark", "%s visible", vuMark);
+        telemetry.update();
+        return vuMark;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOptions.java
@@ -0,0 +1,145 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.content.SharedPreferences;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+/**
+ * Created by Aryoman on 2/16/2016.
+ * revised by JASMINE on 2/17/2016.
+ * revised by Simone on 3/24/2016.
+ */
+
+@TeleOp(name="Autonomous Options", group="Main")
+
+public class AutonomousOptions extends OpMode {
+
+    enum State  {DisplayAll, DisplaySingle}
+
+    private State menuState = State.DisplayAll;
+
+    private SharedPreferences prefs;
+    private SharedPreferences.Editor editor;
+
+    private int selectionIdx = 0;
+
+    // ADD preference names here
+    //   public static final String DELAY_PREF = "delay";
+    //   public static final String START_TILE_PREF = "startTile";
+    private static final String NONE = "none";
+
+    // ADD preference values here
+    // for example:
+    //   private static final String[] DELAYS = {"0 sec", "3 sec", "5 sec", "7 sec"};
+    //   private static final String[] START_POS = {"3rd tile from corner", "4th tile from corner"};
+
+    public static SharedPreferences getSharedPrefs(HardwareMap hardwareMap) {
+        return hardwareMap.appContext.getSharedPreferences("autonomous", 0);
+    }
+
+
+    private static Map<String, String[]> prefMap = new HashMap<>();
+    static {
+        // ADD entries to preference map here
+        // for example:
+        //    prefMap.put(DELAY_PREF, DELAYS);
+        //    prefMap.put(START_TILE_PREF, START_POS);
+    }
+    private static String[] prefKeys = prefMap.keySet().toArray(new String[prefMap.keySet().size()]);
+    static {
+        Arrays.sort(prefKeys);
+    }
+    private static int keyIdx = 0;
+
+
+    private int getIndex(String val, String[] array) {
+        if(array!=null){
+            for (int i = 0; i < array.length; i++) {
+                if (array[i].equals(val)) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public void init() {
+        prefs = getSharedPrefs(hardwareMap);
+
+        editor = prefs.edit();
+        for (String key : prefs.getAll().keySet()) {
+            telemetry.addData(key, prefs.getString(key, NONE));
+        }
+    }
+
+
+    @Override
+    public void loop() {
+        switch (menuState) {
+            case DisplayAll:
+                displayAll();
+                break;
+            case DisplaySingle:
+                displaySingle();
+                break;
+        }
+    }
+
+    private void displayAll () {
+
+        telemetry.clear();
+        telemetry.addData("Choose", "X - accept Y - change");
+        for (String key : prefKeys) {
+            telemetry.addData(key, prefs.getString(key, NONE));
+        }
+        if (gamepad1.y) {
+            while (gamepad1.y) {
+            }
+            menuState = State.DisplaySingle;
+        }
+    }
+
+    private void displaySingle () {
+
+        telemetry.clear();
+        telemetry.addData("Choose", "X - accept Y - change");
+        String key = prefKeys[keyIdx];
+        String[] array = prefMap.get(key);
+        if (key != null) {
+            String prefValue = prefs.getString(key, NONE);
+            selectionIdx = getIndex(prefValue, array);
+            telemetry.addData(key, prefValue);
+        }
+        if (gamepad1.x) {
+            while (gamepad1.x) {
+            }
+            int nextKeyIdx = keyIdx+1;
+            if (nextKeyIdx >= prefKeys.length) {
+                keyIdx = 0;
+                menuState = State.DisplayAll;
+            }
+            else {
+                keyIdx = nextKeyIdx;
+            }
+        }
+        if (gamepad1.y) {
+            while (gamepad1.y) {
+            }
+            selectionIdx++;
+            if (selectionIdx >= array.length) {
+                selectionIdx = 0;
+            }
+            editor.putString(key, array[selectionIdx]);
+            editor.apply();
+        }
+    }
+}
