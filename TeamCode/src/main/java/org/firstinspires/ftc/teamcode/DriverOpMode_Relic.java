d1c2a1b77a4e21aafc5637c2c4fa1f3841a1595d
Newton Busters <newtonbustersftc@gmail.com>
2017-10-29 14:45:21 -0500

    Gavin, Ian: added code to support lift

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverOpMode_Relic.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverOpMode_Relic.java
@@ -2,6 +2,10 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by JASMINE on 10/22/17.
@@ -13,25 +17,74 @@ public class DriverOpMode_Relic extends OpMode {
     private MecanumWheels mecanumWheels;
     private boolean backButtonPressed;
 
+    private DcMotor lift; //DcMotor for the lift
+    private int LIFT_COUNT_MAX = 5000;
+
+    private boolean touchSensorReleased;
+
+    private DigitalChannel touchSensor; //Touch sensor at lowest position on the lift
+
+    Servo leftHand;
+    double LEFT_HAND_IN_POS = 0.63;
+    double LEFT_HAND_OUT_POS = 0.48;
+
+    Servo rightHand;
+    double RIGHT_HAND_IN_POS = 0.25;
+    double RIGHT_HAND_OUT_POS = 0.4;
+
+    Servo jewelArm;
+    double JEWEL_ARM_HOME = 0.72; // home position
+    double JEWEL_ARM_DOWN = 0.08; // down position
+    double JEWEL_ARM_VERTICAL = 0.55; // down position
+
+    Servo jewelKick;
+    double JEWEL_KICK_IN = 0.8; // start (rest) position and counterclockwise kick
+    double JEWEL_KICK_OUT = 0.15; // clockwise kick
+    double JEWEL_KICK_CENTER = 0.47;
+
     @Override
     public void init() {
         mecanumWheels = new MecanumWheels(hardwareMap, telemetry);
         mecanumWheels.powerMotors(0, 0, 0);
+        lift = hardwareMap.dcMotor.get("Lift");
+        lift.setDirection(DcMotorSimple.Direction.REVERSE);
+        lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        touchSensor = hardwareMap.digitalChannel.get("Touch-Sensor");
+        touchSensor.setMode(DigitalChannel.Mode.INPUT);
+        touchSensorReleased = touchSensor.getState();
+        if (!touchSensorReleased) {
+            resetEncoders(lift,true);
 
+        }
+        lift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lift.setPower(0);
+        telemetry();
     }
 
     @Override
     public void start() {
         backButtonPressed = false;
+        leftHand = hardwareMap.servo.get("Left-Hand");
+        setUpServo(leftHand, LEFT_HAND_IN_POS, LEFT_HAND_OUT_POS);
+        rightHand = hardwareMap.servo.get("Right-Hand");
+        setUpServo(rightHand, RIGHT_HAND_IN_POS, RIGHT_HAND_OUT_POS);
+        jewelArm = hardwareMap.servo.get("Jewel-Arm");
+        jewelArm.setPosition(JEWEL_ARM_HOME);
+        jewelKick = hardwareMap.servo.get("Jewel-Kick");
+        jewelKick.setPosition(JEWEL_KICK_IN);
     }
 
     @Override
     public void loop() {
 
+        controlLift();
+        telemetry();
+
+        // DRIVING
 
         //Changing direction from forward to backward and backward to forward
         //Gamepad back button does not work with motorola 3G
-        if (gamepad1.a) {
+        if (gamepad1.y) {
             backButtonPressed = true;
         } else if (backButtonPressed) {
             backButtonPressed = false;
@@ -41,8 +94,6 @@ public class DriverOpMode_Relic extends OpMode {
 
 
         double clockwise = gamepad1.right_stick_x;
-
-
         //We are using robot coordinates
         if (gamepad1.dpad_up || gamepad1.dpad_down || gamepad1.dpad_left || gamepad1.dpad_right) {
             double forward = 0;
@@ -63,19 +114,87 @@ public class DriverOpMode_Relic extends OpMode {
             double right = gamepad1.left_stick_x;
             mecanumWheels.powerMotors(forward, right, clockwise);
         }
+    }
+
+    private void controlLift() {
+
+        // touchSensor.getState==true means the button is NOT PRESSED
+        boolean touchPressed = !touchSensor.getState();
+        if (touchPressed) {
+            if (touchSensorReleased){
+                resetEncoders(lift,false);
+                touchSensorReleased = false;
+            } else {
+                if (lift.getCurrentPosition() < 50 && !lift.getMode().equals(DcMotor.RunMode.RUN_WITHOUT_ENCODER)) {
+                    lift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                }
+            }
+
+        } else {
+            touchSensorReleased = true;
+        }
+
+        if (gamepad2.y && lift.getCurrentPosition()< LIFT_COUNT_MAX) {
+            lift.setPower(0.3);
+        } else if (gamepad2.x && !touchPressed) {
+            lift.setPower(-0.2);
+        } else {
+            lift.setPower(0);
+        }
+    }
+
+    /**
+     * Takes the holding position and release position,
+     * Makes them the minimum and maximum servo positions
+     * After this method, you can pass percentOpen as the servo position.
+     * Holding position would be 0, complete release position would be 1.
+     * @param servo - servo motor
+     * @param inPos - holding position between 0 and 1
+     * @param outPos - releasing position between 0 and 1
+     */
+    public static void setUpServo(Servo servo, double inPos, double outPos) {
+        // scale t0 the range between inPos and outPos
+        double min = Math.min(inPos, outPos);
+        double max = Math.max(inPos, outPos);
+        servo.scaleRange(min, max);
+
+        // make sure in position = 0, out position = 1
+        if (inPos > outPos) {
+            servo.setDirection(Servo.Direction.REVERSE);
+        } else {
+            servo.setDirection(Servo.Direction.FORWARD);
+        }
+
+        setPercentOpen(servo, 0);
+    }
+
+    /**
+     * Sets percent open
+     * @param servo - servo motor to use
+     * @param percentOpen is the number between 0 and 1
+     */
+    public static void setPercentOpen(Servo servo, double percentOpen) {
+        servo.setPosition(percentOpen);
+    }
+
+    void resetEncoders(DcMotor motor,boolean wait) {
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        if (wait){
+            while (motor.getCurrentPosition() > 10) {
+
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    break;
+                }
+            }
+        }
 
     }
 
-    //todo
-//    /**
-//     * @param inPos - servo all the way in position
-//     * @param outPos - servo all the way out position
-//     * @param percentOpen - if inPos is 0.0 and outPos is 1.0
-//     * @return
-//     */
-//    public static double calculateServoPosition(double inPos, double outPos, double percentOpen){
-//
-//    }
-
-
+    void telemetry() {
+        telemetry.addData("lift", lift.getCurrentPosition());
+        telemetry.addData("touch sensor state", touchSensor.getState());
+        telemetry.addData("touch sensor released", touchSensorReleased);
+    }
 }
