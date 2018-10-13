18dbc0eb9d920a0d04f3856c59f6540ca69f1dab
newtonbustersftc <newtonbustersftc@gmail.com>
2018-10-13 14:45:44 -0700

    Samantha: Added PixyCam driver and a test class for it

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PixyCamTest.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+@TeleOp(name = "PixyCamTest")
+public class PixyCamTest extends OpMode {
+
+    PixyCam pixyCam;
+    PixyCam.Block block1;
+    PixyCam.Block block2;
+    ElapsedTime elapsedTime = new ElapsedTime();
+
+    /*
+     * Code to run when the op mode is first enabled goes here
+     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#start()
+     */
+    @Override
+    public void init()
+    {
+        pixyCam = hardwareMap.get(PixyCam.class, "pixycam");
+    }
+
+    /*
+     * This method will be called repeatedly in a loop
+     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#loop()
+     */
+    @Override
+    public void loop()
+    {
+        if (elapsedTime.milliseconds() > 100) // Update every tenth of a second.
+        {
+            elapsedTime.reset();
+            block1 = pixyCam.getBiggestBlock(1);
+            telemetry.addData("Signature 1", block1.toString());
+            block2 = pixyCam.getBiggestBlock(2);
+            telemetry.addData("Signature 2", block2.toString());
+        }
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PixyTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PixyTest.java
@@ -28,9 +28,9 @@ public class PixyTest extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
-//            readPixy(0x51);
-//            readPixy(0x52);
-            readPixy(0x50);
+            readPixy(0x51);
+            readPixy(0x52);
+  //        readPixy(0x50);
 
             telemetry.update();
 
@@ -40,7 +40,7 @@ public class PixyTest extends LinearOpMode {
             // the longer sleep, the less probable the exception
             // if less then 100 ms, it happens in a few seconds
             // if more than 800 ms, it rarely happens
-            sleep(800);
+            sleep(50);
         }
 
     }
@@ -54,52 +54,52 @@ public class PixyTest extends LinearOpMode {
         // read data for given signature
         byte[] pixyData;
 
-            if (signatureAddress == 0x50) {
-                // pixy_LEGO_protocol_1.0.pdf
-                // general mode: query address 0x50
-                // 6 bytes that describe the largest detected block:
-                // byte 0,1: Number of blocks that match the specified signature
-                // byte 2: X value of center of largest detected block, ranging between 0 and 255. An x value of 255 is the far right­side of Pixy’s image.
-                // byte 3: Y value of center of largest detected block, ranging between 0 and 199. A value of 199 is the far bottom­side of Pixy’s image.
-                // byte 4: Width of largest block, ranging between 1 and 255. A width of 255 is the full width of Pixy’s image.
-                // byte 5: Height of largest block, ranging between 1 and 200. A height of 200 is the full height of Pixy’s image.
+        if (signatureAddress == 0x50) {
+            // pixy_LEGO_protocol_1.0.pdf
+            // general mode: query address 0x50
+            // 6 bytes that describe the largest detected block:
+            // byte 0,1: Number of blocks that match the specified signature
+            // byte 2: X value of center of largest detected block, ranging between 0 and 255. An x value of 255 is the far right­side of Pixy’s image.
+            // byte 3: Y value of center of largest detected block, ranging between 0 and 199. A value of 199 is the far bottom­side of Pixy’s image.
+            // byte 4: Width of largest block, ranging between 1 and 255. A width of 255 is the full width of Pixy’s image.
+            // byte 5: Height of largest block, ranging between 1 and 200. A height of 200 is the full height of Pixy’s image.
 
-                pixyData = pixy.read(signatureAddress, 6);
-                int signature = getNumber(pixyData[0], pixyData[1]);
-                int x = 0xff & pixyData[2];
-                int y = 0xff & pixyData[3];
-                int width = 0xff & pixyData[4];
-                int height = 0xff & pixyData[5];
+            pixyData = pixy.read(signatureAddress, 6);
+            int signature = getNumber(pixyData[0], pixyData[1]);
+            int x = 0xff & pixyData[2];
+            int y = 0xff & pixyData[3];
+            int width = 0xff & pixyData[4];
+            int height = 0xff & pixyData[5];
 
-                telemetry.addData("Signature", signature);
-                telemetry.addData("(x, y) ", "(" + x + "," + y + ")");
-                telemetry.addData("(width, height) ", "(" + width + "," + height + ")");
+            telemetry.addData("Signature", signature);
+            telemetry.addData("(x, y) ", "(" + x + "," + y + ")");
+            telemetry.addData("(width, height) ", "(" + width + "," + height + ")");
 
-            } else {
+        } else {
 
-                // pixy_LEGO_protocol_1.0.pdf
-                // signature query: query address 0x51 thru 0x57
-                // 5 bytes that describe the largest detected block for a signature:
-                // byte 0: Number of blocks that match the specified signature
-                // byte 1: X value of center of largest detected block, ranging between 0 and 255. An x value of 255 is the far right­side of Pixy’s image.
-                // byte 2: Y value of center of largest detected block, ranging between 0 and 199. A value of 199 is the far bottom­side of Pixy’s image.
-                // byte 3: Width of largest block, ranging between 1 and 255. A width of 255 is the full width of Pixy’s image.
-                // byte 4: Height of largest block, ranging between 1 and 200. A height of 200 is the full height of Pixy’s image.
+            // pixy_LEGO_protocol_1.0.pdf
+            // signature query: query address 0x51 thru 0x57
+            // 5 bytes that describe the largest detected block for a signature:
+            // byte 0: Number of blocks that match the specified signature
+            // byte 1: X value of center of largest detected block, ranging between 0 and 255. An x value of 255 is the far right­side of Pixy’s image.
+            // byte 2: Y value of center of largest detected block, ranging between 0 and 199. A value of 199 is the far bottom­side of Pixy’s image.
+            // byte 3: Width of largest block, ranging between 1 and 255. A width of 255 is the full width of Pixy’s image.
+            // byte 4: Height of largest block, ranging between 1 and 200. A height of 200 is the full height of Pixy’s image.
 
-                pixyData = pixy.read(signatureAddress, 5);
+            pixyData = pixy.read(signatureAddress, 5);
 
-                int x = 0xff & pixyData[1];
-                int y = 0xff & pixyData[2];
-                int width = 0xff & pixyData[3];
-                int height = 0xff & pixyData[4];
-                int numObjects = 0xff & pixyData[0];
+            int x = 0xff & pixyData[1];
+            int y = 0xff & pixyData[2];
+            int width = 0xff & pixyData[3];
+            int height = 0xff & pixyData[4];
+            int numObjects = 0xff & pixyData[0];
 
-                int sig = signatureAddress - 0x50;
+            int sig = signatureAddress - 0x50;
 
-                telemetry.addData("NumObjects" + sig, numObjects);
-                telemetry.addData("(x, y) " + sig, "(" + x + "," + y + ")");
-                telemetry.addData("(width, height) " + sig, "(" + width + "," + height + ")");
-            }
+            telemetry.addData("NumObjects" + sig, numObjects);
+            telemetry.addData("(x, y) " + sig, "(" + x + "," + y + ")");
+            telemetry.addData("(width, height) " + sig, "(" + width + "," + height + ")");
+        }
 
 
     }
